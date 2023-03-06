ebab3967285f44597d5d12b0ce99a07e29df11f4
Jessica Aldous <jcaldous@Jessicas-MacBook-Air-8.local>
2023-03-06 12:26:57 -0500

    Added some details of the project

--- 1simulation.R
+++ 1simulation.R
@@ -1,7 +1,24 @@
-#####
+##############################
 # Title: 699 Project 3 R code
 # Date created: 3/6/2023
 
 # Date last edited: 3/6/2023
 #     by: Jessica Aldous
-#####
+##############################
+
+#Effect in control: 0%-15%
+
+#Delta: 15%
+
+#type 1 error: 0.05
+#type 2 error: 0.2
+
+
+#Hypothesis we are testing:
+#H0: |Delta|<0.15 
+#Ha: |Delta|>0.15 
+
+
+
+
+
