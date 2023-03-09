3ac6291d100d6e31516b6ad60ef3c8d2131da69f
Jessica Aldous <jcaldous@Jessicas-MacBook-Air-8.local>
2023-03-09 15:29:03 -0500

    updated simulations and tweaking of the sample size

--- 1simulation.R
+++ 1simulation.R
@@ -2,13 +2,14 @@
 # Title: 699 Project 3 R code
 # Date created: 3/6/2023
 
-# Date last edited: 3/6/2023
+# Date last edited: 3/9/2023
 #     by: Jessica Aldous
+#~*~#: Notes to be resolved
 ##############################
 
 #Effect in control: 0%-15%
 
-#Delta: 15%
+#Delta under alternative: 15%
 
 #type 1 error: 0.05
 #type 2 error: 0.2
@@ -16,59 +17,163 @@
 
 #Hypothesis we are testing:
 #H0: Delta=0 
-#Ha: Delta =/= 0
+#Ha: Delta =/=0 
 
-#general calculations
-library(pwr)
-power.prop.test(p1=0.15, p2=0.30, 
-                sig.level = 0.05,power=0.80, 
-                alternative = "two.sided")
-#121 per group, 242 overall
-
-source("trial_sim_bernoulli.R")
-#####
-#Initial n caluclation as a starting point
-target_size <- 0.05;
-target_power <- 0.80;
-assumed_pA = 0.15;
-assumed_pB = 0.3;
-assumed_delta <- assumed_pB - assumed_pA;
-assumed_sigmasq_A <- assumed_pA * (1 - assumed_pA);
-assumed_sigmasq_B <- assumed_pB * (1 - assumed_pB);
-ratio_n_B_n_A <- 1;
+######Initial n calculation as a starting point#####
+type_1_error <- 0.05; #type 1 error
+target_power <- 0.80; #power
+assumed_pA = 0.15; #proportion of SOC 
+assumed_pB = assumed_pA+0.15; #Porpotion of ND314
+assumed_delta <- assumed_pB - assumed_pA; #difference in proportion
+assumed_sigmasq_A <- assumed_pA * (1 - assumed_pA); 
+assumed_sigmasq_B <- assumed_pB * (1 - assumed_pB); 
+ratio_n_B_n_A <- 1; #balanced arms
 required_n_A <-
-  (qnorm(1 - target_size/2) +
+  (qnorm(1 - type_1_error/2) + 
      qnorm(target_power))**2 /
   assumed_delta**2 *
   (assumed_sigmasq_A + assumed_sigmasq_B /
-     ratio_n_B_n_A);
-required_n_B <- ratio_n_B_n_A * required_n_A;
-alpha_fin=(0.05-alpha_int)*(1-alpha_int);
+     ratio_n_B_n_A); #general sample size for arm A
+required_n_B <- ratio_n_B_n_A * required_n_A; #general sample size for arm B
+alpha_int=0.02 #rejection threshold at stage 1 (<0.05)
+alpha_fin=(0.05-alpha_int)/(1-alpha_int); #rejection threshold at stage 2 (<0.05)
+#~*~#alpha_fin needs some tweaking, working on that rn
 
-check.pwr<-trial_sim_2stg(10000,118/2,118/2,assumed_pA,assumed_pB,0.01,alpha_fin)
+#####Number of simulations needed calculations######
+#Number of simulations needed calculations
+moe=0.01 #margin of error
+
+#conservative: p=0.5
+S_con= (qnorm(0.975)/moe)**2 *(.5)*(.5)
+S_con
+#9604
+#Under null: p=0.05
+S_H0= (qnorm(0.975)/moe)**2 *(.05)*(.95)
+S_H0
+#1825
+#Under alternative: p=0.8
+S_HA= (qnorm(0.975)/moe)**2 *(.8)*(.2)
+S_HA
+#6147
+#Use 10,000 to be conservative 
+nsim=10000
+#####Check initial starting point power and t1e#####
+check.pwr<-trial_sim_2stg(nsim,118/2,118/2,assumed_pA,assumed_pB,alpha_int,alpha_fin) #simulate under Ha
 mean(check.pwr$decision)#power 0.67 (too small)
 
-check.alp<-trial_sim_2stg(10000,118/2,118/2,assumed_pA,assumed_pA,0.01,alpha_fin)
+check.alp<-trial_sim_2stg(nsim,118/2,118/2,assumed_pA,assumed_pA,alpha_int,alpha_fin) #simulate under H0
 mean(check.alp$decision)# 0.03 (too small)
-#####
-#####
-#what do we need to get a power of 0.8?
+####what n do we need to get a power of 0.8?#####
+dat<-data.frame(nsim=numeric(0),n_stg=numeric(0),S_t=numeric(0))
+for(j in 1:100){
+  start_sim = Sys.time()
 for (i in 59:200){
-  check.pwr<-trial_sim_2stg(1000,i,i,assumed_pA,assumed_pB,0.01,alpha_fin)
+  check.pwr<-trial_sim_2stg(nsim,i,i,assumed_pA,assumed_pB,alpha_int,alpha_fin)
   S_t <-mean(check.pwr$decision)#power 0.92 (too small)
   if (S_t>=0.8){
-    print(
-      list(n_stg=i,
-           S_t=S_t,
-           CI=S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / 1000)
-           )
-    )
+    dat_add<-data.frame(nsim=j,n_stg=i,S_t=S_t)
+    dat<-rbind(dat,dat_add)
+    # print(
+    #   list(n_stg=i,
+    #        S_t=S_t,
+    #        CI=S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / nsim)
+    #        )
+    # )
     break
-  }else{
-    print(paste(i,"0.8 not reached:",S_t))
   }
+}
+  print(paste("nsim",j,Sys.time() - start_sim))
+}
+summary(dat$n_stg)#82.8
+mean(dat$S_t)+qnorm(c(0.025,0.975)) * sqrt(var(dat$S_t)) #looks okay
+#####what alpha_fin do we need to get a type 1 of about 0.05?#####
+t1e.dat<-data.frame(nsim=numeric(0),n_stg=numeric(0),S_t=numeric(0))
+
+for(j in 1:1e4){
+  start_sim = Sys.time()
+  sim<-trial_sim_2stg(nsim,59,59,assumed_pA,assumed_pA,alpha_int,alpha_fin) #run sim
+  new.alpha.fin<-quantile(sim$pvalues_stg2_quant, probs=c(alpha_fin), na.rm=T) #get new alpha
+  new.decision<-(check.alp$pvalues_stg1<alpha_int)*I(check.alp$pvalues_stg1 < alpha_int)+ 
+    (check.alp$pvalues_stg2<new.alpha.fin)*(1-I(check.alp$pvalues_stg1 < alpha_int));
+      check.t1e<-mean(new.decision, na.rm = T) #check t1e
+      dat_add<-data.frame(nsim=j,alpha.fin=new.alpha.fin,S_t=check.t1e)%>%mutate(flag=(S_t>0.048 & S_t<0.052));
+      t1e.dat<-rbind(t1e.dat,dat_add)
+      # print(
+      #   list(n_stg=i,
+      #        S_t=S_t,
+      #        CI=S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / nsim)
+      #        )
+      # )
+  print(paste("nsim",j,Sys.time() - start_sim))
+}
+hist(t1e.dat$alpha.fin)
+summary(t1e.dat$S_t)
+summary(t1e.dat$alpha.fin); #mean alpha.final adjusted
+mean(t1e.dat$S_t); #mean t1e is about right
+mean(t1e.dat$S_t)+qnorm(c(0.025,0.975)) * sqrt(var(t1e.dat$S_t)) #95% CI of t1e
+
+#set alpha.fin to median from above
+new.alpha.fin<-round(median(t1e.dat$alpha.fin),4); #median alpha.final adjusted: 0.0376
+
+check.alp<-trial_sim_2stg(nsim,59,59,assumed_pA,assumed_pA,alpha_int,new.alpha.fin)
+S_t <-mean(check.alp$decision)#alpha=0.0499 # okay
+S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / nsim) #c(0.046, 0.054)
+
+#####What n_stage do we need to achieve:t1e~0.05, pwr~0.8, pr_stop>0.40 ?#####
+n.new<-data.frame(nsim=numeric(0),n_stg=numeric(0),S_t=numeric(0),t1e=numeric(0),
+                  pr_stop=numeric(0))
+for(j in 1:100){
+  start_sim = Sys.time()
+  for (i in 59:82){
+    check.pwr<-trial_sim_2stg(nsim,i,i,assumed_pA,assumed_pB,alpha_int,new.alpha.fin)
+    check.t1e<-trial_sim_2stg(nsim,i,i,assumed_pA,assumed_pA,alpha_int,new.alpha.fin)
+    S_t <-mean(check.pwr$decision)#power     
+    t1e <-mean(check.t1e$decision)#type 1 error
+    pr_stop<-mean(check.pwr$stopped_int)#power at int
+    if (S_t>=0.8 & (t1e<0.050 & t1e>0.045) & (pr_stop>=0.4)){
+      dat_add<-data.frame(nsim=j,n_stg=i,S_t=S_t,t1e=t1e,pr_stop=pr_stop)
+      n.new<-rbind(n.new,dat_add)
+      # print(
+      #   list(n_stg=i,
+      #        S_t=S_t,
+      #        CI=S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / nsim)
+      #        )
+      # )
+      break
+    }
   }
-check.alp<-trial_sim_2stg(1000,86,86,assumed_pA,assumed_pA,0.01,alpha_fin)
-S_t <-mean(check.alp$decision)#alpha=0.1139
-S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / 1000) #type 1 error is too low
-#####
+  print(paste("nsim",j,Sys.time() - start_sim))
+}
+
+ggplot(n.new, aes(x=n_stg,y=S_t))+geom_smooth()+geom_point()
+summary(n.new$S_t)#range (0.8-0.86)
+summary(n.new$t1e); #range (0.044-0.0500)
+summary(n.new$pr_stop); #range (0.40-0.43)
+
+summary(n.new$n_stg); #range (63-74)
+mean(n.new$S_t)+qnorm(c(0.025,0.975)) * sqrt(var(n.new$S_t)) #95% CI of power 0.79-0.82
+mean(n.new$t1e)+qnorm(c(0.025,0.975)) * sqrt(var(n.new$t1e)) #95% CI of t1e 0.046-0.051
+mean(n.new$pr_stop)+qnorm(c(0.025,0.975)) * sqrt(var(n.new$pr_stop)) #95% CI of pr_stop 0.39-0.44
+
+#####take the mean (rounded): 68#####
+check.alp<-trial_sim_2stg(nsim,68,68,assumed_pA,assumed_pA,alpha_int,new.alpha.fin)
+S_t <-mean(check.alp$decision)#alpha=0.0497  #reasonable
+S_t +qnorm(c(0.025,0.975)) * sqrt(S_t * (1-S_t) / nsim) #c(0.042, 0.051)(reasonable)
+
+check.pwr<-trial_sim_2stg(nsim,68,68,assumed_pA,assumed_pB,alpha_int,new.alpha.fin)
+mean(check.pwr$decision)#reasonable: 0.8348
+mean(check.pwr$decision) +qnorm(c(0.025,0.975)) * sqrt(mean(check.pwr$decision) * (1-mean(check.pwr$decision)) / nsim) 
+mean(check.pwr$stopped_int)#reasonable: 0.4212
+
+#####FINAL VALUES#####
+alpha_int #interim alpha threshold: 0.02
+new.alpha.fin #final alpha threshold: 0.0375
+
+n_stg=68 #n/4
+4*n_stg #max n: 272, blocks of size 2,4,8,34
+#####Different scenarios:Null's#####
+
+
+#####Different scenarios:Non Null's#####
+
+#####Different scenarios:Additional#####
--- trial_sim_2stg.R
+++ trial_sim_2stg.R
@@ -7,51 +7,66 @@ trial_sim_2stg <- function(n_sim, #number of simulations
                                 alpha_fin, #final alpha level
                                 seed = sample(.Machine$integer.max, 1)) {
   set.seed(seed)
-  start_sim = Sys.time();
+  start_sim = Sys.time(); #time it takes to run the simulation
   stg1_data_A <- 
-    rbinom(n_sim, n_A, pA) / n_A;
+    rbinom(n_sim, n_A, pA) / n_A; #stage 1, arm A
   stg1_data_B <-
-    rbinom(n_sim, n_B, pB) / n_B;
-  estimated_sigmasq_A <- stg1_data_A * (1 -  stg1_data_A);
-  estimated_sigmasq_B <- stg1_data_B * (1 -  stg1_data_B);
-  t_stat <-
+    rbinom(n_sim, n_B, pB) / n_B;#stage 1, arm B
+  stg1_all<- (stg1_data_A*n_A+stg1_data_B*n_B) / (n_A+n_B); #all stage 1
+  # estimated_sigmasq_A <- stg1_data_A * (1 -  stg1_data_A);
+  # estimated_sigmasq_B <- stg1_data_B * (1 -  stg1_data_B);
+  # t_stat <-
+  #   (stg1_data_A - stg1_data_B) / 
+  #   sqrt(estimated_sigmasq_A / n_A + estimated_sigmasq_B / n_B);
+  z_stat <-
     (stg1_data_A - stg1_data_B) / 
-    sqrt(estimated_sigmasq_A / n_A + estimated_sigmasq_B / n_B);
-  df_approx <- 
-    (estimated_sigmasq_A/n_A + estimated_sigmasq_B/n_B)^2 / 
-    ((estimated_sigmasq_A/n_A)^2/(n_A-1) + 
-       (estimated_sigmasq_B/n_B)^2/(n_B-1));
-  pvalues_stg1_t = 2 * pt(q = abs(t_stat), 
-                          df = df_approx, 
+    sqrt(stg1_all*(1-stg1_all)*(1 / n_A + 1 / n_B)); #stg 1 test stat (Z)
+  # df_approx <- 
+  #   (estimated_sigmasq_A/n_A + estimated_sigmasq_B/n_B)^2 / 
+  #   ((estimated_sigmasq_A/n_A)^2/(n_A-1) + 
+  #      (estimated_sigmasq_B/n_B)^2/(n_B-1));
+  # pvalues_stg1_t = 2 * pt(q = abs(t_stat), 
+  #                         df = df_approx, 
+  #                         lower.tail = FALSE);
+  pvalues_stg1_z = 2 * pnorm(q = abs(z_stat), 
                           lower.tail = FALSE)
   
-  stg2_data_B <- (stg1_data_B*n_B+rbinom(n_sim, n_B, pB))/(2*n_B);
-  stg2_data_A <- (stg1_data_A*n_A+rbinom(n_sim, n_A, pA))/(2*n_A);
+  stg2_data_B <- (stg1_data_B*n_B+rbinom(n_sim, n_B, pB))/(2*n_B);#if there is a stage 2, arm A
+  stg2_data_A <- (stg1_data_A*n_A+rbinom(n_sim, n_A, pA))/(2*n_A);#if there is a stage 2, arm A
+  stg2_all<-(stg2_data_B*2*n_B+stg2_data_A*2*n_A)/(2*n_A+2*n_B);
   estimated_sigmasq_A2 <- stg2_data_A * (1 -  stg2_data_A);
   estimated_sigmasq_B2 <- stg2_data_B * (1 -  stg2_data_B);
-    t_stat_2 <-
-      (stg2_data_A - stg2_data_B) / 
-      sqrt(estimated_sigmasq_A2 / (2*n_A) + estimated_sigmasq_B2 / (2*n_B));
-    df_approx_2 <- 
-      (estimated_sigmasq_A2/(2*n_A) + estimated_sigmasq_B2/(2*n_B))^2 / 
-      ((estimated_sigmasq_A2/(2*n_A))^2/((2*n_A)-1) + 
-         (estimated_sigmasq_B2/(2*n_B))^2/((2*n_B)-1));
-    pvalues_stg2_t = 2 * pt(q = abs(t_stat_2), 
-                            df = df_approx_2, 
-                            lower.tail = FALSE)
-  
+    # t_stat_2 <-
+    #   (stg2_data_A - stg2_data_B) / 
+    #   sqrt(estimated_sigmasq_A2 / (2*n_A) + estimated_sigmasq_B2 / (2*n_B));
+    # df_approx_2 <- 
+    #   (estimated_sigmasq_A2/(2*n_A) + estimated_sigmasq_B2/(2*n_B))^2 / 
+    #   ((estimated_sigmasq_A2/(2*n_A))^2/((2*n_A)-1) + 
+    #      (estimated_sigmasq_B2/(2*n_B))^2/((2*n_B)-1));
+  z_stat_2 <-
+    (stg2_data_A - stg2_data_B) / 
+    sqrt(stg2_all*(1-stg2_all)*(1 /(2*n_A) + 1 /(2*n_B)));
+    # pvalues_stg2_t = 2 * pt(q = abs(t_stat_2), 
+    #                         df = df_approx_2, 
+    #                         lower.tail = FALSE)
+    pvalues_stg2_z = 2 * pnorm(q = abs(z_stat_2), 
+                             lower.tail = FALSE)
+    pvalues_stg2_quant= ifelse(pvalues_stg1_z < alpha_int, NA,pvalues_stg2_z)
     return(list(alphas=c(alpha_int,alpha_fin),
-                stopped_int=pvalues_stg1_t < alpha_int,
-                pvalues_stg1=pvalues_stg1_t,
-                pvalues_stg2=pvalues_stg2_t,
-                decision_1=pvalues_stg1_t<alpha_int,
-                decision_2=pvalues_stg2_t<alpha_fin,
-                pvalues_t = pvalues_stg2_t*I(pvalues_stg1_t < alpha_int)+ 
-                  pvalues_stg1_t*(1-I(pvalues_stg1_t < alpha_int)),
-                dec_t = t_stat*I(pvalues_stg1_t < alpha_int)+ 
-                  t_stat_2*(1-I(pvalues_stg1_t < alpha_int)),
-                decision= (pvalues_stg1_t<alpha_int)*stopped_int+ 
-                  (pvalues_stg2_t<alpha_fin)*(1-stopped_int),
+                stopped_int=pvalues_stg1_z < alpha_int,
+                pvalues_stg1=pvalues_stg1_z,
+                pvalues_stg2=pvalues_stg2_z,
+                pvalues_stg2_quant=pvalues_stg2_quant,
+                decision_1=pvalues_stg1_z<alpha_int,
+                decision_2=pvalues_stg2_z<alpha_fin,
+                pvalues_z = pvalues_stg2_z*I(pvalues_stg1_z < alpha_int)+ 
+                  pvalues_stg1_z*(1-I(pvalues_stg1_z < alpha_int)),
+                dec_z = (z_stat)*stopped_int+ 
+                  (z_stat_2)*(1-stopped_int),
+                decision = (pvalues_stg1<alpha_int)*I(pvalues_stg1 < alpha_int)+ 
+                  (pvalues_stg2<alpha_fin)*(1-I(pvalues_stg1 < alpha_int)),
+                # decision= (pvalues_stg1_t<alpha_int)*stopped_int+ 
+                #   (pvalues_stg2_t<alpha_fin)*(1-stopped_int),
                 runtime = Sys.time() - start_sim
         )
     )
